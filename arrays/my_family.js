89bce75b32d7d95a302c4377a3043f7a4caeeb8e
DevFaulk <faulkdev@gmail.com>
2023-11-01 11:36:06 -0400

    add course search exercise

--- arrays/my_family.js
+++ arrays/my_family.js
@@ -1,4 +1,4 @@
-let myFamily = ["Teddy Faulk", "Momma Faulk", "Daddy Faulk", "Sister Faulk"];
-for (index = 0; index < myFamily.length; index++) {
-  console.log(myFamily[index]);
-}
+// let myFamily = ["Teddy Faulk", "Momma Faulk", "Daddy Faulk", "Sister Faulk"];
+// for (index = 0; index < myFamily.length; index++) {
+//   console.log(myFamily[index]);
+// }
